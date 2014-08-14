85994d35fb59265a13de4d5b01892fa08d54c8aa
robotBones <kim.marquardt@gmail.com>
2014-08-13 19:56:49 -0700

    helperrrrs

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var lazy = require('lazy');
+var httpHelp = require('../web/http-helpers.js');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -22,20 +24,62 @@ exports.initialize = function(pathsObj){
   });
 };
 
+// redirect helper
+exports.redirect = function(filename){
+  var staticLoading = path.join(__dirname, filename);
+  httpHelp.serveAssets(res, staticLoading, function(data){
+    res.writeHead(200, headers);
+    res.write(data);
+    console.log("redirecting");
+    res.end();
+  });
+};
+
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(){
+var sitesFile = path.join(__dirname, '../archives/sites.txt');
+
+exports.readListOfUrls = function(website){
+// read every line of file
+  var websiteList;
+  var myFileAsText;
+  fs.readFile(sitesFile, function readFileCB(error, data){
+    if(data){
+      myFileAsText = data.toString();
+      websiteList = myFileAsText.split("\n");
+    }
+    exports.isUrlInList(websiteList, website);
+  });
+
 };
 
-exports.isUrlInList = function(){
+exports.isUrlInList = function(list, website){
+  if(!_(list).contains(website)){
+    exports.addUrlToList(website);
+  } else {
+    exports.isURLArchived(website);
+  }
+
 };
 
-exports.addUrlToList = function(){
+exports.addUrlToList = function(website){
+  // add novel POSTed website
+  fs.appendFile(sitesFile, "\n" + website);
 };
 
-exports.isURLArchived = function(){
+exports.isURLArchived = function(website){
+  var archived = path.join(exports.path['archivedSites'],website); // this returns a path as a string
+  fs.exists(archived, function isURLArchivedCB(exists){
+    if(exists){
+      console.log("website is indeed archived", archived);
+      exports.redirect(archived);
+    } else {
+      console.log("WTF");
+    }
+  });
 };
 
+// assuming this is the function to archive websites on the list of urls.
 exports.downloadUrls = function(){
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,71 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelp = require('./http-helpers.js');
+var qs = require('querystring');
 // require more modules/folders here!
 
+var headers = httpHelp.headers;
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log('in handleRequest()');
+  console.log('request url is: ', req.url);
+
+  var getSite = function(){
+    console.log('in getSite()');
+    var staticIndex = path.join(__dirname, './public/index.html');
+    console.log(staticIndex);
+    httpHelp.serveAssets(res, staticIndex, function(data){
+      console.log('inside of callback');
+      res.writeHead(200, headers);
+      console.log('reached end of getSite()');
+      res.write(data);
+      res.end();
+    });
+  };
+
+var redirect = function(filename){
+  var staticLoading = path.join(__dirname, filename);
+  httpHelp.serveAssets(res, staticLoading, function(data){
+    res.writeHead(200, headers);
+    res.write(data);
+    console.log("redirecting");
+    res.end();
+  });
 };
+
+  var makeArchive = function(){
+    var body = '';
+    req.on('data', function(data) {
+      body += data;
+    });
+    req.on('end', function(){
+      var post = qs.parse(body);
+      console.log('post: ', post.url);
+      // work on this
+      archive.readListOfUrls(post.url);
+    });
+    archive.redirect('./public/loading.html');
+  };
+
+  var optionsResponse = function(){
+    console.log('in optionsResponse');
+    console.log('headers: ', headers);
+    res.writeHead(200, headers);
+  };
+
+  var verbMap = {
+    "GET": getSite,
+    "POST": makeArchive,
+    "OPTIONS": optionsResponse
+  }
+
+  var action = verbMap[req.method];
+  // if verb has a map
+  if(typeof action === 'function'){
+    console.log('request method recognized: ', req.method);
+    action(res);
+  }
+  // make sure res is end();
+  // res.end(archive.paths.list);
+};
+
